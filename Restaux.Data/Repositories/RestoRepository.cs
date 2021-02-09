6e2f5c467e5868d625ef155d704b3881db4f028e
DialloAbdou <abdoudiallo3826@yahoo.fr>
2021-02-09 23:03:21 +0100

    ajout repoUtilisateur & reResto

---
+++ Restaux.Data/Repositories/RestoRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaux.Core.Models;
+using Restaux.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaux.Data.Repositories
+{
+    public class RestoRepository : Repository<Resto>, IRestoRepository
+    {
+        private RestoDbContext RestoDbContext {
+            get { return Context as RestoDbContext; }
+        }
+        public RestoRepository(RestoDbContext restoDbContext) : base(restoDbContext)
+        {
+            
+
+        }
+        public async Task<IEnumerable<Resto>> GetAllRestoAsync()
+        {
+            return await RestoDbContext.Restos.ToListAsync();
+        }
+
+        public async Task<Resto> GetRestoByIdAsync(int id)
+        {
+            return await RestoDbContext.Restos.FirstOrDefaultAsync(r => r.Id == id);
+        }
+    }
+}
---
+++ Restaux.Data/Repositories/UtilisateurRepository.cs
@@ -0,0 +1,34 @@
+﻿using Restaux.Core.Models;
+using Restaux.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaux.Data.Repositories
+{
+    public class UtilisateurRepository : Repository<Utilisateur>, IUtilisateurRepository
+    {
+
+        private RestoDbContext RestoDbContext
+        {
+            get
+            {
+                return Context as RestoDbContext;
+            }
+        }
+        public UtilisateurRepository()
+        {
+
+        }
+        public Task<IEnumerable<Utilisateur>> GetAllUtisateurAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Utilisateur> GetUtilisateurAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
