2cb1ec5102e90f8f9ea42dbb43a2537103f30166
DialloAbdou <abdoudiallo3826@yahoo.fr>
2021-02-24 12:33:58 +0100

    premiere partie reposUtilisateur

--- Restaux.Core/Repositories/IUtilisateurRepository.cs
+++ Restaux.Core/Repositories/IUtilisateurRepository.cs
@@ -9,12 +9,13 @@ namespace Restaux.Core.Repositories
     public interface IUtilisateurRepository : IRepository<Utilisateur>
     {
         Task<Utilisateur> Authentificate(string nom, string mpd);
-        Task<IEnumerable<Utilisateur>> GetAllUtisateurAsync();
-        Task<Utilisateur> GetUtilisateurAsync(int id);
+        Task<Utilisateur> GetWithRestoByIdAsync(int id);
         bool AdejatVoter(int idSondage, int idUtil);
         Task<Utilisateur> CreateUtillisateur(Utilisateur utilisateur, string mdp);
         void UpdateUtilisateur(Utilisateur utilisateur, string mdp = null);
         void DeleteUtilisateur(int id);
+        Task<IEnumerable<Utilisateur>> GetAllWithRestoAsync();
+
 
     }
 }
--- Restaux.Data/Repositories/UtilisateurRepository.cs
+++ Restaux.Data/Repositories/UtilisateurRepository.cs
@@ -3,6 +3,7 @@ using Restaux.Core.Models;
 using Restaux.Core.Repositories;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,28 +20,69 @@ namespace Restaux.Data.Repositories
             }
         }
         public UtilisateurRepository(RestoDbContext restoDbContext)
-            :base(restoDbContext)
+            : base(restoDbContext)
         {
 
         }
-        public async Task<IEnumerable<Utilisateur>> GetAllUtisateurAsync()
+
+        public async Task<Utilisateur> Authentificate(string nom, string mdp)
         {
-            return await RestoDbContext.Utilisateurs.ToListAsync();
+            return await RestoDbContext
+                         .Utilisateurs
+                         .FirstOrDefaultAsync(u => u.Prenom == nom && u.MotDePasse == mdp);
         }
 
-        public async Task<Utilisateur> GetUtilisateurAsync(int id)
+        public async Task<Utilisateur> GetWithRestoByIdAsync(int id)
         {
-            return await RestoDbContext.Utilisateurs.SingleOrDefaultAsync(u => u.Id == id);
-        }
-
-        public Task<Utilisateur> Authentificate(string nom, string mpd)
-        {
-            throw new NotImplementedException();
+            return await RestoDbContext
+                       .Utilisateurs
+                       .Include(u => u.Restos)
+                       .SingleOrDefaultAsync(u => u.Id == id);
         }
 
         public bool AdejatVoter(int idSondage, int idUtil)
         {
+            return false;
+        }
+
+        public async Task<Utilisateur> CreateUtillisateur(Utilisateur utilisateur, string mdp)
+        {
             throw new NotImplementedException();
+        }
+
+        public void UpdateUtilisateur(Utilisateur utilisateur, string mdp = null)
+        {
+            var user = RestoDbContext.Utilisateurs.Find(utilisateur.Id);
+            if (user == null) throw new Exception("utilisateur n'existe pas");
+            if(utilisateur.Prenom != user.Prenom)
+            {
+                if (RestoDbContext.Utilisateurs.Any(u => u.Prenom == utilisateur.Prenom))
+                    throw new Exception("Utisateur " + utilisateur.Prenom + " il est tjs prise");
+            }
+
+            user.Prenom = utilisateur.Prenom;
+            user.MotDePasse = mdp;
+            RestoDbContext.Utilisateurs.Update(user);
+
+            // === Voir la suite Apres===
+         
+
+        }
+
+        public void DeleteUtilisateur(int id)
+        {
+            var user = RestoDbContext.Utilisateurs.FirstOrDefault(u => u.Id == id);
+            if (user == null) throw new Exception("utilisateur n'existe pas");
+            RestoDbContext.Utilisateurs.Remove(user);
+        }
+
+        public async Task<IEnumerable<Utilisateur>> GetAllWithRestoAsync()
+        {
+            return await RestoDbContext
+                .Utilisateurs
+                .Include(u => u.Restos)
+                .ToListAsync();
+
         }
     }
 }
