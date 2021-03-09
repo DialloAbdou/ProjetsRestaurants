cc1ac0179499660a338f06c14e6f9b1d0e573d42
DialloAbdou <abdoudiallo3826@yahoo.fr>
2021-03-09 21:02:01 +0100

    implementation Service

---
+++ Restaux.Services/Services/RestoService.cs
@@ -0,0 +1,50 @@
+﻿using Restaux.Core;
+using Restaux.Core.Models;
+using Restaux.Core.services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaux.Services.Services
+{
+    public class RestoService : IRestoService
+    {
+        private readonly IUnitOfWork _UnitOfWork;
+
+        public RestoService(IUnitOfWork unitOfWork)
+        {
+            _UnitOfWork = unitOfWork;
+        }
+
+        public async Task<Resto> CreateResto(Resto resto)
+        {
+            await _UnitOfWork.Restos.AddAsync(resto);
+            await _UnitOfWork.CommitAsync();
+            return resto;
+        }
+
+        public async Task DeleteResto(Resto resto)
+        {
+            _UnitOfWork.Restos.Remove(resto);
+            await _UnitOfWork.CommitAsync();
+        }
+
+        public async Task<IEnumerable<Resto>> GetAllArtist()
+        {
+            return await _UnitOfWork.Restos.GetAllAsync();
+        }
+
+        public async Task<Resto> GetRestoById(int id)
+        {
+            return await _UnitOfWork.Restos.GetByIdAsync(id);
+        }
+
+        public async Task UpdateResto(Resto restoUpdate, Resto resto)
+        {
+            restoUpdate.Nom = resto.Nom;
+            restoUpdate.Telephone = resto.Telephone;
+            await _UnitOfWork.CommitAsync();
+        }
+    }
+}
---
+++ Restaux.Services/Services/SondageService.cs
@@ -0,0 +1,43 @@
+﻿using Restaux.Core;
+using Restaux.Core.Models;
+using Restaux.Core.services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaux.Services.Services
+{
+    public class SondageService : ISondageService
+    {
+        private readonly IUnitOfWork _UnitOfWork;
+
+        public SondageService(IUnitOfWork unitOfWork)
+        {
+            _UnitOfWork = unitOfWork;
+        }
+
+        public async Task<int> CreateSondage()
+        {
+            var sondage = new Sondage { Date = DateTime.Now };
+            await _UnitOfWork.Sondages.AddAsync(sondage);
+            await _UnitOfWork.CommitAsync();
+            return sondage.Id;
+        }
+
+        public async Task<IEnumerable<Sondage>> GetAll()
+        {
+            return await _UnitOfWork.Sondages.GetAllAsync();
+        }
+
+        public Task<Sondage> GetSondage(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Resultat>> ObtenirLesResultats(int idSondage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
