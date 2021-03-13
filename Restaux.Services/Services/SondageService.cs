9cd45a2e40fcc2ff764cace862d261cb81411ca2
DialloAbdou <abdoudiallo3826@yahoo.fr>
2021-03-13 18:44:53 +0100

    implementation sondage &VoteService

---
+++ Restaux.Services/Services/SondageService.cs
@@ -0,0 +1,39 @@
+﻿using Restaux.Core;
+using Restaux.Core.Models;
+using Restaux.Core.services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaux.Services.Services
+{
+    public class SondageService : ISondageService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SondageService( IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<int> CreateSondage()
+        {
+            var sondage = new Sondage { Date = DateTime.Now };
+            _unitOfWork.Sondages.CreateSondage();
+            await _unitOfWork.CommitAsync();
+            return sondage.Id;
+        }
+
+        public async Task<IEnumerable<Sondage>> GetAll()
+        {
+            return await _unitOfWork.Sondages.GetAllAsync();
+        }
+
+        public async Task<Sondage> GetSondage(int id)
+        {
+            return await _unitOfWork.Sondages.GetWithVoteByIdAsync(id);
+
+        }
+    }
+}
---
+++ Restaux.Services/Services/VoteService.cs
@@ -0,0 +1,46 @@
+﻿using Restaux.Core;
+using Restaux.Core.Models;
+using Restaux.Core.services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaux.Services.Services
+{
+     public class VoteService : IVoteService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public VoteService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+
+        }
+
+            public async Task AddVote(int idSondage, int idResto, int idUtilisateur)
+            {
+                   _unitOfWork.Votes.
+            }
+
+        public bool AdejatVoter(int idSondage, int idUtil)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Vote>> GetAllVote()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Vote> GetVoteById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Resultat>> ObtenirLesResultats(int idSondage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
